385511fa3d16421d0c5170e9e62473b06119efd1
JonatasElias <jonatas.santos96@etec.sp.gov.br>
2020-07-29 22:24:47 -0300

    Commit final

---
+++ src/clientes/controle/view-cliente.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+    $('#table-cliente').on('click', 'button.btn-view', function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Visualização do cliente')
+
+        let idcliente = `idcliente=${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            data: idcliente,
+            url: 'src/clientes/modelo/view-cliente.php',
+            success: function(dado) {
+                if (dado.tipo == "success") {
+                    $('.modal-body').load('src/clientes/visao/form-cliente.html', function() {
+                        $('#nome').val(dado.dados.nome)
+                        $('#nome').attr('readonly', 'true')
+                        $('#email').val(dado.dados.email)
+                        $('#email').attr('readonly', 'true')
+                        $('#telefone').val(dado.dados.telefone)
+                        $('#telefone').attr('readonly', 'true')
+                        $('#dataagora').val(dado.dados.datacriacao)
+
+                        if (dado.dados.ativo == "N") {
+                            $('#ativo').removeAttr('checked')
+                        }
+
+                        $('#ativo').attr('readolnly', 'true')
+
+                    })
+                    $('.btn-save').hide()
+                    $('.btn-update').hide()
+                    $('#modal-cliente').modal('show')
+                } else {
+                    Swal.fire({
+                        title: 'appAulaDS',
+                        text: dado.mensagem,
+                        type: dado.tipo,
+                        confirmButtonText: 'OK'
+                    })
+                }
+            }
+        })
+
+    })
+
+})
---
+++ src/clientes/modelo/all-cliente.php
@@ -0,0 +1,42 @@
+<?php
+
+    include('../../banco/conexao.php');
+
+    if($conexao){
+
+
+        $sql = "SELECT idcliente, nome FROM clientes WHERE ativo = 'S' ";
+        $resultado = mysqli_query($conexao, $sql);
+
+        if($resultado && mysqli_num_rows($resultado) > 0){
+            
+            $dadosCliente = array();
+            while($linha = mysqli_fetch_assoc($resultado)){
+                $dadosCliente[] = array_map('utf8_encode', $linha);
+            }
+
+            $dados = array(
+                "tipo" =>"success",
+                "mensagem" => "",
+                "dados" => $dadosCliente
+            );
+
+        } else{
+            $dados = array(
+                "tipo" => "info",
+                "mensagem" => "Não foi possível localizar os clientes.",
+                "dados" => array()
+            );
+        }
+
+        mysqli_close($conexao);
+
+    } else{
+        $dados = array(
+            "tipo" => "error",
+            "mensagem" => "Não possível conecar ao banco de dados",
+            "dados" => array()
+        );
+    }
+
+echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
